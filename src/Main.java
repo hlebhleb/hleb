a18e4399e800cbd211b52d736f2e0e1c0eb8d7c0
hleb <9204320426c@gmail.com>
2025-10-17 23:36:53 +0300

    commit 2

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,2 @@
-import mylib.collections.MyHashSet;
-import mylib.collections.MyHashMultiSet;
-
 public class Main {
-    public static void main(String[] args) {
-        MyHashSet<String> set = new MyHashSet<>();
-        set.add("apple");
-        System.out.println(set);
-
-        MyHashMultiSet<String> multi = new MyHashMultiSet<>();
-        multi.add("apple");
-        multi.add("orange");
-        System.out.println(multi);
-    }
 }
